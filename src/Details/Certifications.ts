4dfcb307807e4cefba953d5349305ffa0bdeb16d
Sanjai451 <sanjaikumarr451@gmail.com>
2025-02-20 18:59:57 +0530

    created portfolio

--- src/Details/Certifications.ts
+++ src/Details/Certifications.ts
@@ -12,21 +12,15 @@ export const certificationsData = [
       imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmWc1f4G5ZTK-OTpPLfh5BZx74tqLTBEjO9w&s',
     },
     {
-      title: 'Python Certification - Infosys Springboard ',
-      date: 'August 2024',
-      description: 'Python programming and object oriented programming concepts.',
-      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmWc1f4G5ZTK-OTpPLfh5BZx74tqLTBEjO9w&s',
-    },
-    {
-      title: ' Cybersecurity Analysis & Design - Nasscom',
-      date: 'August 2024',
-      description: 'Concepts in Cybersecurity and various attacks',
-      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmWc1f4G5ZTK-OTpPLfh5BZx74tqLTBEjO9w&s',
-    },
-    {
       title: 'Programming with javascript - META',
       date: 'January 2024',
       description: 'JavaScript programming and application development with libraries and Testing using JEST.',
+      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmWc1f4G5ZTK-OTpPLfh5BZx74tqLTBEjO9w&s',
+    },
+    {
+      title: 'Python Certification - Infosys Springboard ',
+      date: 'August 2024',
+      description: 'Python programming and object oriented programming concepts.',
       imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmWc1f4G5ZTK-OTpPLfh5BZx74tqLTBEjO9w&s',
     },
     {
@@ -39,6 +33,30 @@ export const certificationsData = [
       title: 'Foundations of User Experience (UX) Design - GOOGLE',
       date: 'March 2023',
       description: 'Designing of creative and modular UX design.',
+      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmWc1f4G5ZTK-OTpPLfh5BZx74tqLTBEjO9w&s',
+    },
+    {
+      title: 'Skill Test - HACKERRANK',
+      date: '2023 - 2024',
+      description: 'Passed Skill test in various fields such as Java, JavaScript and more.',
+      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmWc1f4G5ZTK-OTpPLfh5BZx74tqLTBEjO9w&s',
+    },
+    {
+      title: 'Skill Test - HACKERRANK',
+      date: '2023 - 2024',
+      description: 'Passed Skill test in various fields such as Java, JavaScript and more.',
+      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmWc1f4G5ZTK-OTpPLfh5BZx74tqLTBEjO9w&s',
+    },
+    {
+      title: 'Skill Test - HACKERRANK',
+      date: '2023 - 2024',
+      description: 'Passed Skill test in various fields such as Java, JavaScript and more.',
+      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmWc1f4G5ZTK-OTpPLfh5BZx74tqLTBEjO9w&s',
+    },
+    {
+      title: 'Skill Test - HACKERRANK',
+      date: '2023 - 2024',
+      description: 'Passed Skill test in various fields such as Java, JavaScript and more.',
       imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmWc1f4G5ZTK-OTpPLfh5BZx74tqLTBEjO9w&s',
     },
     {
