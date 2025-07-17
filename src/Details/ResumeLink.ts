ac9b11188897b61e2397905a56b11e1de2803462
Sanjai Kumar R <145897247+Sanjai451@users.noreply.github.com>
2025-07-17 09:35:04 +0530

    Update ResumeLink.ts

--- src/Details/ResumeLink.ts
+++ src/Details/ResumeLink.ts
@@ -1,2 +1,2 @@
-const RESUME_LINK = "https://drive.google.com/file/d/1hryj0F4oQJyOUQkprWHV2jGETdS6biT3/view?usp=drive_link"
+const RESUME_LINK = "https://drive.google.com/file/d/1mPnjAQq-5QmaNdosfmddl9ZNzGx9X7en/view?usp=sharing"
 export default RESUME_LINK;
